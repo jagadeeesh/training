74e9a71caa9c841845e86e79d859c58d5273fbb5
jagadeesh <k.jagadeesh123@gmail.com>
2016-01-03 23:38:07 +0530

    send with singleton method

--- send_usage.rb
+++ send_usage.rb
@@ -7,3 +7,13 @@ arr = [10,20,30,40]
 SendUsage.new("+",arr)
 SendUsage.new("*",arr)
 
+
+
+obj = Array.new([2,3,4])
+def obj.foldl(op)
+	inject{|r,i| r ? r.send(op,i) : i}
+end
+p obj.foldl("+")
+p obj.foldl("-")
+p obj.foldl("*")
+
